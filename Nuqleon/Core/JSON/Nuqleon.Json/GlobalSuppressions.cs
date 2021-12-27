52258b0aeb80aa45780610af7586a0068abdd889
Alex@Threadripper <alex@strackers.at>
2021-12-27 14:30:13 +0100

    suppress all new warnings that show up with dot net 6.0.100 so it builds again

--- Nuqleon/Core/JSON/Nuqleon.Json/GlobalSuppressions.cs
+++ Nuqleon/Core/JSON/Nuqleon.Json/GlobalSuppressions.cs
@@ -2,6 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT License.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
+
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "~N:System.Collections.Generic")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "~N:Nuqleon.Json.Serialization")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "~N:Nuqleon.Json.Parser")]
+[assembly: SuppressMessage("Performance", "CA1846:Prefer 'AsSpan' over 'Substring'", Justification = "<Pending>", Scope = "member", Target = "~M:Nuqleon.Json.Parser.Tokenizer.Tokenize(System.Boolean)~System.Collections.Generic.IEnumerator{Nuqleon.Json.Parser.Token}")]
--- Nuqleon/Core/LINQ/Nuqleon.Linq.CompilerServices/GlobalSuppressions.cs
+++ Nuqleon/Core/LINQ/Nuqleon.Linq.CompilerServices/GlobalSuppressions.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT License.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
+
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "~N:System", Justification = "Merging with BCL namespaces.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "~N:System.Linq.Expressions", Justification = "Merging with BCL namespaces.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "~N:System.Linq.CompilerServices.TypeSystem", Justification = "Separate namespace to allow for using aliases.")]
@@ -11,3 +13,4 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling", Scope = "member", Target = "~M:System.Linq.Expressions.ExpressionUnsafe.#cctor")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1725:ParameterNamesShouldMatchBaseDeclaration", MessageId = "0#", Scope = "member", Target = "~M:System.Linq.CompilerServices.StructuralSubstitutingTypeComparator.EqualsStructural(System.Type,System.Type)~System.Boolean", Justification = "Base class naming is not clear.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1725:ParameterNamesShouldMatchBaseDeclaration", MessageId = "1#", Scope = "member", Target = "~M:System.Linq.CompilerServices.StructuralSubstitutingTypeComparator.EqualsStructural(System.Type,System.Type)~System.Boolean", Justification = "Base class naming is not clear.")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:Nuqleon.Reflection.Emit.MethodBuilder.MakeGenericMethod(System.Type[])~System.Reflection.MethodInfo")]
